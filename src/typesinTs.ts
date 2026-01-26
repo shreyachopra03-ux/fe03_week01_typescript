fbc9c4842e31a821389225c15d4435b982a2e11c
shreya <chopra.shreya003@gmail.com>
2026-01-27 01:02:42 +0530

    feat: Type inferencing & Type annotation

--- src/typesinTs.ts
+++ src/typesinTs.ts
@@ -2,7 +2,7 @@
 let drink = "chai"
 let cups = Math.random() > 0.5 ? 10 : 5
 
-// Type annotation -> khud se Ts ko btaana ki kya type hoga variable / fn ka
+// Type annotation -> khud se Ts ko btaana ki kya type hoga variable / fn ka 
 let chaiFlavour : string = "masala chai"
 chaiFlavour = 'giner tea'
 
