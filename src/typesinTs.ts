de03c0f6bf92aed86272364d60cf02149c23e060
Shreya Chopra <chopra.shreya03@gmail.com>
2026-02-23 19:57:20 +0530

    Types In TS

--- src/typesinTs.ts
+++ src/typesinTs.ts
@@ -1,10 +1,15 @@
 // Type inferencing -> jab Ts khud se variable / fn ka type guess krleta hai 
+
 let drink = "chai"
 let cups = Math.random() > 0.5 ? 10 : 5
 
 // Type annotation -> khud se Ts ko btaana ki kya type hoga variable / fn ka 
+
 let chaiFlavour : string = "masala chai"
-chaiFlavour = 'giner tea'
+// chaiFlavour can be over-written as well because both the data types are same (string)
+chaiFlavour = 'ginger tea'
 
 let chaiOrder : boolean
+
+let chaiPrice: number
 
