6e91c35ff0e6d75c04e231a1fc5a652d37f10b65
Shreya Chopra <chopra.shreya03@gmail.com>
2026-02-23 19:15:01 +0530

    Basics Of TS

---
+++ src/Basic.ts
@@ -0,0 +1,7 @@
+// 
+function greet(person: string) : string {
+    return `Hello ${person}, Welcome to ChaiCode`;
+}
+
+const username: string = "Chai aur typescript";
+console.log(greet(username));
